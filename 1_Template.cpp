1e8cd18d6425d883e9d847149337a63e6dbb2e88
Niraj Amrutkar <>
2022-07-12 14:19:27 +0530

    Temp Updated

--- 1_Template.cpp
+++ 1_Template.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+const ll MOD = 1e9 + 7;
+
 
 void solution(){
-    
-    
+    fast_cin();
+
+
 }
 
 int main(){
+    fast_cin();
     ll t ;
     cin>>t;
     while(t--){
